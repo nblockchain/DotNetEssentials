e972f1317434946fb6f51dd6c57b548869b5ff41
Diego Calleja <diegocg@gmail.com>
2019-10-12 19:52:43 +0200

    Test with a new script

--- scripts/nuget.fsx
+++ scripts/nuget.fsx
@@ -11,8 +11,6 @@ open System.Linq
 open FSX.Infrastructure
 open Process
 
-// we need to download nuget.exe because `dotnet pack` doesn't support using standalone (i.e.
-// without a project association) .nuspec files, see https://github.com/NuGet/Home/issues/4254
 let nugetDownloadUri = Uri "https://dist.nuget.org/win-x86-commandline/v4.5.1/nuget.exe"
 Network.DownloadFile nugetDownloadUri
 
@@ -33,36 +31,41 @@ let GetIdealNugetVersion (initialVersion: string) =
                                initialVersion dateSegment gitSegment
     finalVersion
 
-let rootDir = DirectoryInfo(Path.Combine(__SOURCE_DIRECTORY__, ".."))
-let nugetExe = Path.Combine(rootDir.FullName, "nuget.exe") |> FileInfo
-let nuspecFile = Path.Combine(rootDir.FullName, "build", "JsonRpcSharp.nuspec") |> FileInfo
+let PackAndMaybeUpload (packageName: string) =
+    let rootDir = DirectoryInfo(Path.Combine(__SOURCE_DIRECTORY__, ".."))
+    let nugetExe = Path.Combine(rootDir.FullName, "nuget.exe") |> FileInfo
+    let nuspecFile = Path.Combine(rootDir.FullName, sprintf "%s.nuspec" packageName)
+                     |> FileInfo
 
-let nugetVersion = GetIdealNugetVersion "0.10"
-let nugetPackCmd =
-    {
-        Command = nugetExe.FullName
-        Arguments = sprintf "pack %s -Version %s"
-                            nuspecFile.FullName nugetVersion
-    }
+    let nugetVersion = GetIdealNugetVersion "0.10"
+    let nugetPackCmd =
+        {
+            Command = nugetExe.FullName
+            Arguments = sprintf "pack %s -Version %s"
+                                nuspecFile.FullName nugetVersion
+        }
 
-Process.SafeExecute (nugetPackCmd, Echo.All) |> ignore
+    Process.SafeExecute (nugetPackCmd, Echo.All) |> ignore
 
-let packageName = "JsonRpcSharp"
-let packageFile = sprintf "%s.%s.nupkg" packageName nugetVersion
-let argsPassedToThisScript = Misc.FsxArguments()
-if argsPassedToThisScript.Length <> 1 then
-    Console.WriteLine "NUGET_API_KEY not passed to script, skipping upload..."
-else
-    let nugetApiKey = argsPassedToThisScript.First()
-    let githubRef = Environment.GetEnvironmentVariable "GITHUB_REF"
-    if githubRef <> "refs/heads/master" then
-        Console.WriteLine "Branch different than master, skipping upload..."
+    let packageFile = sprintf "%s.%s.nupkg" packageName nugetVersion
+    let argsPassedToThisScript = Misc.FsxArguments()
+    if argsPassedToThisScript.Length <> 1 then
+        Console.WriteLine "NUGET_API_KEY not passed to script, skipping upload..."
     else
-        let defaultNugetFeedUrl = "https://api.nuget.org/v3/index.json"
-        let nugetPushCmd =
-            {
-                Command = "dotnet"
-                Arguments = sprintf "nuget push %s -k %s -s %s"
-                                    packageFile nugetApiKey defaultNugetFeedUrl
-            }
-        Process.SafeExecute (nugetPushCmd, Echo.All) |> ignore
+        let nugetApiKey = argsPassedToThisScript.First()
+        let githubRef = Environment.GetEnvironmentVariable "GITHUB_REF"
+        if githubRef <> "refs/heads/master" then
+            Console.WriteLine "Branch different than master, skipping upload..."
+        else
+            let defaultNugetFeedUrl = "https://api.nuget.org/v3/index.json"
+            let nugetPushCmd =
+                {
+                    Command = "dotnet"
+                    Arguments = sprintf "nuget push %s -k %s -s %s"
+                                        packageFile nugetApiKey defaultNugetFeedUrl
+                }
+            Process.SafeExecute (nugetPushCmd, Echo.All) |> ignore
+
+
+PackAndMaybeUpload "ZXing.Net.Xamarin"
+PackAndMaybeUpload "ZXing.Net.Xamarin.Forms"
