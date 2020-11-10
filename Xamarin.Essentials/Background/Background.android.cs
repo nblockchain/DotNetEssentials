72150c08f3b141f8c682bbc5a744d98f1149be1b
Afshin Arani <afshinarani79@gmail.com>
2020-11-10 15:15:09 +0330

    separate init & start functions in android impl
    Init function (which receives the Context) and start function are
    executed in different parts of the app (first one from Android project
    and second one from Xamarin.Form common project) and they should be separated

--- Xamarin.Essentials/Background/Background.android.cs
+++ Xamarin.Essentials/Background/Background.android.cs
@@ -7,8 +7,17 @@ namespace Xamarin.Essentials.Background
 {
     public static partial class Background
     {
+        static ContextWrapper context;
+
+        public static void Init(ContextWrapper ctx)
+        {
+            context = ctx;
+        }
+
         public static void StartBackgroundService(ContextWrapper context)
         {
+            if (context == null)
+                throw new InvalidOperationException("Background service is not initialized yet");
             var intent = new Intent(context, typeof(BackgroundService));
             context.StartService(intent);
         }
